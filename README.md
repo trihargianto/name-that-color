47f9c3c31c93d7a7c0a24bfb5809358dca766992
Tri Hargianto <trihargianto@gmail.com>
2023-12-21 19:11:25 +0700

    feat(app): add color group name

--- README.md
+++ README.md
@@ -11,12 +11,13 @@ npm install @trihargianto/ntcjs --save
 ## Getting Started
 
 ```javascript
-const ntc = require('@trihargianto/ntcjs');
+const ntc = require("@trihargianto/ntcjs");
 
-const n_match = ntc.name('#6195ED');
+const n_match = ntc.name("#6195ED");
 n_rgb = n_match[0]; // RGB value of closest match
 n_name = n_match[1]; // Text string: Color name
 n_exactmatch = n_match[2]; // True if exact color match
+n_exactmatch = n_match[3]; // Text string: Color group name
 
 console.log(n_match); // [ '#6495ED', 'Cornflower Blue', false ]
 ```
--- test/index.js
+++ test/index.js
@@ -1,11 +1,17 @@
-const test = require('tape');
-const tapSpec = require('tap-spec');
-const ntc = require('../src/index');
+const test = require("tape");
+const tapSpec = require("tap-spec");
+const ntc = require("../src/index");
 
 // make tape output look pretty
 test.createStream().pipe(tapSpec()).pipe(process.stdout);
 
-test('ntc test', function(t) {
-    t.plan(1);
-    t.ok(ntc, 'ntc exists and is defined');
+test("ntc test", function (t) {
+  t.plan(1);
+  t.ok(ntc, "ntc exists and is defined");
+});
+
+test("able to convert hex to color name", function (t) {
+  t.plan(2);
+  t.equal(ntc.name("#35312C")[1], "Acadia");
+  t.equal(ntc.name("#35312C")[3], "Brown");
 });
